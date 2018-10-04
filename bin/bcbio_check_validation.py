1a50b76da2bd94cc1806505b5a00bfebe4262b67
chapmanb <chapmanb@50mail.com>
2018-10-04 05:33:29 -0400

    Modernize validation checking for CWL combined outputs
    - Handles naming in CWL combined outputs with multiple callers.
    - Avoids failures when expected is the same as output
    - Floor for minimum differences to handle tiny inputs
    - Correctly integrate with scattered CWL for runners like Cromwell that
      won't merge types with the back-compatible specification.

--- bin/bcbio_check_validation.py
+++ bin/bcbio_check_validation.py
@@ -13,17 +13,18 @@ def main(base_output, *cur_outputs):
     # if we have some logic about what to use with multiple
     cur_output = cur_outputs[0]
     threshold = 0.0025  # 0.25% different relative to initial output
+    default_min_flex = 5  # Defaul minimum count flexibility
     base_vals = _validation_to_vals(base_output)
     cur_vals = _validation_to_vals(cur_output)
-    allowed = _get_allowed_differences(base_vals, threshold)
+    allowed = _get_allowed_differences(base_vals, threshold, default_min_flex)
     results = {"Steps": {}}
     details = []
     for k, base_v in base_vals.items():
-        if base_v > 0:
-            cur_v = cur_vals.get(k, 0)
+        cur_v = cur_vals.get(k, 0)
+        if base_v > 0 or cur_v > 0:
             k_allowed = allowed[tuple(k[:3])]
             diff = abs(cur_v - base_v)
-            result = diff < k_allowed
+            result = diff <= k_allowed
             results["Steps"]["_".join(list(k))] = result
             details.append(list(k) + [base_v, cur_v, diff, k_allowed, result])
     results["Overall"] = all(results["Steps"].values())
@@ -37,13 +38,13 @@ def main(base_output, *cur_outputs):
         for d in details:
             writer.writerow(d)
 
-def _get_allowed_differences(vals, threshold):
+def _get_allowed_differences(vals, threshold, default_min_flex):
     """Retrieve allowed number of changes relative to TP counts.
     """
     allowed = {}
     for (sample, caller, vartype, metric), count in vals.items():
         if metric == "tp":
-            allowed[(sample, caller, vartype)] = int(count * threshold)
+            allowed[(sample, caller, vartype)] = max(int(count * threshold), default_min_flex)
     return allowed
 
 def _validation_to_vals(val_output):
@@ -52,7 +53,7 @@ def _validation_to_vals(val_output):
         reader = csv.reader(in_handle)
         header = reader.next()
         for val in (dict(zip(header, xs)) for xs in reader):
-            curkey = (val["sample"], val["caller"], val["vtype"], val["metric"])
+            curkey = (val["sample"], val["caller"], val["variant.type"], val["category"])
             out[curkey] = int(val["value"])
     return out
 
